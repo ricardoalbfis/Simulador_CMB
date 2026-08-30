8f9fe201ab101c1e1a5136b2b9702b5874050f09
Ricardo Albrecht <ricardo.alb.fis@gmail.com>
2026-08-30 13:12:09 -0300

    Atualizacao automatica do projeto CMB

--- atualizar_site.bat
+++ atualizar_site.bat
@@ -2,7 +2,7 @@
 echo Sincronizando com o GitHub e atualizando o Streamlit...
 git add .
 git commit -m "Atualizacao automatica do projeto CMB"
-git push origin main
+git push -u origin main
 echo.
 echo Tudo pronto! O site ja esta sendo atualizado.
 pause
