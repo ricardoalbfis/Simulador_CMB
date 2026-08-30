16d5c7ef3b701a9b1d45b04776670076846a66fd
Ricardo Albrecht <ricardo.alb.fis@gmail.com>
2026-08-30 14:44:40 -0300

    dddd

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ st.write("Desenvolvido pelo **Ricardo Albrecht**")
 st.divider() # Cria uma linha de separação
 
 # Mensagem de boas-vindas
-st.markdown("""
+#st.markdown("""
 
 #👈 **Abra o menu lateral (setinha no canto superior esquerdo)** para escolher e navegar entre os simuladores disponíveis.
 
 ---
-**Atualmente disponível no menu:**
+#**Atualmente disponível no menu:**
 #- **1. Espectro CMB:** Simulador termodinâmico da Radiação Cósmica de Fundo (Cosmologia).
-""")
+#""")
