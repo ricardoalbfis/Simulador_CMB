caee94e4c0a56b148658927c6fc375c038b0f06f
Ricardo Albrecht <ricardo.alb.fis@gmail.com>
2026-08-30 13:00:16 -0300

    Primeiro envio do projeto

---
+++ tualizar_site.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Sincronizando com o GitHub e atualizando o Streamlit...
+git add .
+git commit -m "Atualizacao automatica do projeto CMB"
+git push origin main
+echo.
+echo Tudo pronto! O site ja esta sendo atualizado.
+pause
